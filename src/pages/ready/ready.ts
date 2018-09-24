290a9ba4a5ef0e86f5d29260f0f3990e5dadf2bd
14300 <43529572+14300@users.noreply.github.com>
2018-09-24 16:44:59 +1200

    Base

---
+++ src/pages/ready/ready.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-ready',
+  templateUrl: 'ready.html'
+})
+export class ReadyPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
