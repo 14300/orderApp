0bb83ad44ef1b74854710398b88a83e4381bdc2f
14300 <43529572+14300@users.noreply.github.com>
2018-09-24 18:04:50 +1200

    test

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -15,17 +15,46 @@ export class ListPage {
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['restaurant'];
 
     this.items = [];
-    for (let i = 1; i < 11; i++) {
+    
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
+        title:"Noodle Soup",
+        note:"$10.00",
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
-    }
+
+      this.items.push({
+        title:"Noodle Salad",
+        note:"$10.00",
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+
+      this.items.push({
+        title: "Spring Rolls",
+        note: "$6.00",
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+      
+      this.items.push({
+        title:"Dumplings",
+        note:"$6.00",
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+
+      this.items.push({
+        title:"Wontons",
+        note:"$6.00",
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+
+      this.items.push({
+        title:"Salad Bowl",
+        note:"$6.00",
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+
   }
 
   itemTapped(event, item) {
@@ -35,3 +64,4 @@ export class ListPage {
     });
   }
 }
+ 
